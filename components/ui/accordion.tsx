64c4c065c19bc195cc6d3a8c91ad82db0fdd6a29
Animesh Alang <aalang@ucsc.edu>
2024-11-12 21:00:06 -0800

    2

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -14,7 +14,7 @@ const AccordionItem = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AccordionPrimitive.Item
     ref={ref}
-    className={cn("border-b", className)}
+    className={cn("border-b", className ?? "")} // Use a default empty string if className is undefined
     {...props}
   />
 ))
@@ -29,8 +29,8 @@ const AccordionTrigger = React.forwardRef<
       ref={ref}
       className={cn(
         "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
-        className
-      )}
+        className ?? ""
+      )} // Use a default empty string if className is undefined
       {...props}
     >
       {children}
@@ -48,8 +48,8 @@ const AccordionContent = React.forwardRef<
     ref={ref}
     className={cn(
       "overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down",
-      className
-    )}
+      className ?? ""
+    )} // Use a default empty string if className is undefined
     {...props}
   >
     <div className="pb-4 pt-0">{children}</div>
